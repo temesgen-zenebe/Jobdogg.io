513c9fcd1f153853afabe1530862f81cb514b5bc
Temesgen T Zenebe <temf2006@gmail.com>
2023-08-10 02:21:02 -0700

    updating urls

--- JobdoggUserInfo/urls.py
+++ JobdoggUserInfo/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+#from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ContactInfo/', include('userContactInfo.urls')),
+    path('', include('userContactInfo.urls')),
 ]
+
+
+
+"""
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('userContactInfo.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)"""
