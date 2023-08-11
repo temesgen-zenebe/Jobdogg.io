b3b475bc04450b03bcad730523bb7c1add2849d6
Temesgen T Zenebe <temf2006@gmail.com>
2023-08-11 11:49:57 -0700

    crating all success Confirmation and templates

--- JobdoggUserInfo/settings.py
+++ JobdoggUserInfo/settings.py
@@ -38,9 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # Third-party
+    'crispy_forms',
+    'crispy_bootstrap5',
+    
+    #locale app
     'userContactInfo.apps.UsercontactinfoConfig',
    
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- userContactInfo/urls.py
+++ userContactInfo/urls.py
@@ -6,14 +6,17 @@ from userContactInfo.views import (
     CustomerContactInformationCreateView,
     CustomerContactInformationUpdateView,
     CustomerContactInformationDeleteView,
+    SuccessConfirmationView,
 )
 
 app_name = 'userContactInfo'
 
 urlpatterns = [
+    path('', CustomerContactInformationCreateView.as_view(), name='customer-create'),
+    path('successConfirmation/', SuccessConfirmationView.as_view(), name='success-Confirmation'),
     path('contactInformation/', CustomerContactInformationListView.as_view(), name='customer-list'),
     path('contactInformation/<slug:slug>/', CustomerContactInformationDetailView.as_view(), name='customer-detail'),
-    path('contactInformation/create/', CustomerContactInformationCreateView.as_view(), name='customer-create'),
     path('contactInformation/<slug:slug>/update/', CustomerContactInformationUpdateView.as_view(), name='customer-update'),
     path('contactInformation/<slug:slug>/delete/', CustomerContactInformationDeleteView.as_view(), name='customer-delete'),
+    
 ]
